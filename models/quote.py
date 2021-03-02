8422eb7ea145c88821defaf2647fecc66e81f4ab
guillaume <guillaume.lefur29000@gmail.com>
2021-03-02 16:24:04 +0100

    [DONE] Added tags class | Created testing environment | renamed DB [WIP] Wrote tests | endpoints for tags

--- models/quote.py
+++ models/quote.py
@@ -1,22 +1,41 @@
 from db import db
+from models.quote_to_tag import quote_to_tags
 
 
 class QuoteModel(db.Model):
-    __tablename__ = 'quotes'
+    __tablename__ = 'Quote'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80))
-    text = db.Column(db.String(80))
+    text = db.Column(db.String)
+    author = db.Column(db.String)
+    book = db.Column(db.String)
 
-    def __init__(self, name, text):
-        self.name = name
+    tags = db.relationship(
+        'TagModel',
+        secondary=quote_to_tags,
+        lazy='subquery',
+        backref=db.backref('quotes', lazy=True)
+    )
+
+    def __init__(self, text: str, author: str = None, book: str = None):
         self.text = text
+        self.author = author
+        self.book = book
 
     def json(self):
-        return {'text': self.text}
+        return {
+            'id': self.id,
+            'text': self.text,
+            'author': self.author,
+            'book': self.book
+        }
 
     @classmethod
-    def find_by_name(cls, name):
+    def find_by_id(cls, _id: int) -> 'QuoteModel':
+        return cls.query.filter_by(id=_id).first()
+
+    @classmethod
+    def find_by_name(cls, name) -> 'QuoteModel':
         return cls.query.filter_by(name=name).first()
 
     def save_to_db(self):
--- resources/quote.py
+++ resources/quote.py
@@ -1,30 +1,29 @@
 from flask_restful import Resource, reqparse
 # from flask_jwt import jwt_required
-from models.quote import QuoteModel
+from models.quote import QuoteModel, quote_to_tags
 
 
 class Quote(Resource):
     parser = reqparse.RequestParser()
-    parser.add_argument('text',
-                        type=str,
-                        required=True,
-                        help="This field cannot be left blank!"
-                        )
+    parser.add_argument(
+        'text',
+        type=str,
+        required=True,
+        help="This field cannot be left blank!"
+    )
 
     # @jwt_required()
-    def get(self, name):
-        quote = QuoteModel.find_by_name(name)
+    def get(self, _id):
+        quote = QuoteModel.find_by_id(_id)
         if quote:
             return quote.json()
         return {'message': 'quote not found'}, 404
 
-    def post(self, name):
-        if QuoteModel.find_by_name(name):
-            return {'message': "A quote with name '{}' already exists.".format(name)}, 400
-
+    # Should be deleted in the future
+    def post(self, text):
         data = Quote.parser.parse_args()
 
-        quote = QuoteModel(name, **data)
+        quote = QuoteModel(text, **data)
 
         try:
             quote.save_to_db()
@@ -33,28 +32,64 @@ class Quote(Resource):
 
         return quote.json(), 201
 
-    def delete(self, name):
-        quote = QuoteModel.find_by_name(name)
+    def delete(self, _id):
+
+        quote = QuoteModel.find_by_id(_id)
         if quote:
             quote.delete_from_db()
-            return {'message': 'quote deleted.'}
+            return {'message': 'quote deleted.'}, 200
         return {'message': 'quote not found.'}, 404
 
-    def put(self, name):
+    def put(self, _id):
+
         data = Quote.parser.parse_args()
 
-        quote = QuoteModel.find_by_name(name)
-
+        quote = QuoteModel.find_by_id(_id)
+        print(data)
+        print(quote)
         if quote:
             quote.text = data['text']
         else:
-            quote = QuoteModel(name, **data)
+            quote = QuoteModel(**data)
 
         quote.save_to_db()
 
-        return quote.json()
+        return quote.json(), 200
 
 
 class QuoteList(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument(
+        'text',
+        type=str,
+        required=True,
+        help="This field cannot be left blank!"
+    )
+    parser.add_argument(
+        'author',
+        type=str,
+        required=False,
+        help="This field can be left blank!"
+    )
+    parser.add_argument(
+        'book',
+        type=str,
+        required=False,
+        help="This field can be left blank!"
+    )
+
     def get(self):
         return {'quotes': list(map(lambda x: x.json(), QuoteModel.query.all()))}
+
+    def post(self):
+        data = QuoteList.parser.parse_args()
+        quote = QuoteModel(**data)
+
+        try:
+            quote.save_to_db()
+        except:
+            return {"message": "An error occurred inserting the quote."}, 500
+
+        return quote.json(), 201
+
+
